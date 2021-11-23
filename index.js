9eee1a1fb3396519bf7aac1509d0e3be57847a49
PulidoJavier <javierp.pulido@gmail.com>
2021-11-23 08:07:42 -0600

    S30 Middlewares con express

--- index.js
+++ index.js
@@ -8,12 +8,37 @@ import Express from 'express'
 //Crear una instancia de Express
 const app = Express();  //(req,res,next)=> {} event handler
 
+//Registrando el primer middleware
+app.use((req, res, next)=>{
+    //registrando un mensaje en el log
+    console.log("Estoy en el middleware 1");
+    next()//Dar la instruccion de Pasar al siguiente Midleware
+});
+
+//Registrando el segundo middleware
+app.use((req, res, next)=>{
+    console.log("Estoy en el middleware 2");
+    next() //Dar la instruccion de Pasar al siguiente Midleware
+});
+
+//Registrando el tercer middleware
+app.use((req, res)=>{
+    console.log("Estoy en el middleware 3");
+    console.log("Emitiendo Respuesta al cliente");
+   res.send("<h1> Mi respuesta </h1>\n Hola!");
+});
 
 /** 
  * codigo de Emojies
  * Ref: https://www.w3schools.com/charsets/ref_emoji.asp
  */
 
+//Poniendo a escuchar la app de express
+app.listen(3000,'0.0.0.0',()=>{
+    console.log("Servidor escuchando en http://0.0.0.0:3000");
+})
+
+/*
 // 2.- Crear servidor
 // cb (callback) es una *funcion* que se ejecutara
 // ante cualquier peticion de un recurso a nuestro server
@@ -23,6 +48,6 @@ const server = http.createServer(app);
 // le paso un callback que escribira en la consola
 // cuando el servidor este escuchando
 // Mi IP 192.168.1.64:3000
-server.listen(3000, '192.168.100.64', () => {
-    console.log("Servidor escuchando en http://192.168.100.64:3000");
+server.listen(3000, '0.0.0.0', () => {  
 });
+*/
